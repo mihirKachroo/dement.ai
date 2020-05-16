5755b96b6d518384e2a0923e2dc6737d18ead791
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-05-16 17:27:56 -0400

    das

--- Web App/screens/Analytics.js
+++ Web App/screens/Analytics.js
@@ -1,39 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Text, Image, View } from "react-native";
+import React from "react";
+import { Text } from "react-native";
 
 export default function Analytics() {
-  const [data, setData] = useState(null);
-  async function getData() {
-    const data = await fetch("https://tohack.herokuapp.com/", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify("amountSpentDaily"),
-    });
-    const obj = await data.json();
-    return (obj);
-  }
-  function loadImage(blob) {
-    const reader = new FileReader();
-    let dataURL;
-    reader.addEventListener("load", function () {
-      dataURL = reader.result;
-    }, false);
-    if (blob) {
-      reader.readAsDataURL(blob);
-    }
-    return dataURL;
-  }
-  useEffect(() => {
-    getData().then((fetched) => setData(fetched.results.results));
-  }, []);
-  return (
-    <View>
-      {data ? (
-        <Image style={{ width: 50, height: 50 }} source={{ uri: loadImage(data) }} />
-      ) : null}
-    </View>
-  );
+  return <Text>A</Text>;
 }
