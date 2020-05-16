e35bf12da161e44d1a22b788a54d6c02c1909e60
Veer Gadodia <veergadodia24@gmail.com>
2020-05-16 18:36:21 -0400

    updated algorithm

--- sentence-similarity/main.py
+++ sentence-similarity/main.py
@@ -1,6 +1,7 @@
 import text_similarity
 
-percentage = float(text_similarity.token_set_ratio(
+percentage = text_similarity.token_set_ratio(
     "A couple are playing with a young child outside",
-    "A couple playing with a little boy on the beach"))/100
+    "A couple playing with a little boy on the beach")
+percentage = float(percentage)/100
 print(percentage)
