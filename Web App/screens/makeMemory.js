bced9e5095a015842094c9ac13e432ca1e2adcd1
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-05-16 22:02:29 -0400

    bar added

--- Web App/screens/makeMemory.js
+++ Web App/screens/makeMemory.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { Text, TextInput, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import HeaderBar from "../components/HeaderBar";
 import CameraPage from './camera.page';
 
 export default function makeMemory() {
-  const [value, onChangeText] = React.useState('Useless Placeholder');
+  const [value, onChangeText] = React.useState('Add a sentence describing this memory');
   return (
-    <View style={{ flex: 1 }}>
+<View style={{ flex: 1 }}>
+<HeaderBar pageName={"Make a Memory"} />
+
       <TextInput
       style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
       onChangeText={text => onChangeText(text)}
       value={value}
     />
-      <HeaderBar pageName={"Make a Memory"} />
             <CameraPage />
     </View>
   );
