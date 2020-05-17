3f94d7b7edb234771e9989d2a30f9de4caf68d92
mihirKachroo <60433268+mihirKachroo@users.noreply.github.com>
2020-05-17 09:15:47 -0400

    dsa

--- Web App/screens/Budgeting.js
+++ Web App/screens/Budgeting.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Text, View, Image, StyleSheet } from "react-native";
 import { TextInput, Button } from "react-native-paper";
 import HeaderBar from "../components/HeaderBar";
+import AlertExample from './alert_example.js'
 
 export default function makeMemory() {
-  placeSubmitHandler = () => {  
-    alert("button clicked")  
-};  
   const [value, onChangeText] = React.useState('Describe this image again');
   return (
 <View style={{ flex: 1 }}>
@@ -25,11 +23,10 @@ export default function makeMemory() {
         style={styles.stretch}
         source={require('./random.png')}
       />
-    </View>
-    <Button  
-                            title="Test a Memory"  
-                            onPress={this.placeSubmitHandler}  
-                    /> 
+    </View>   
+    <br></br>   
+    <AlertExample />
+
     </View>
   );
 }
